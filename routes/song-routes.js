728cb97eaacc96572225d360b79e6ffdb7ed80b5
lilniblet <rithik.p.2025@computing.smu.edu.sg>
2026-03-14 21:36:47 +0800

    updated images and started on song manager

--- routes/song-routes.js
+++ routes/song-routes.js
@@ -1,8 +1,30 @@
 const express = require("express");
 const router = express.Router();
 
+//Temporary data
+songs = {
+    1:{
+        name:"Hills",
+        artist: "weeknd",
+        avg_review: 5,
+        album_cover: "/images/hills.png"
+    },
+    2:{
+        name:"Back to me",
+        artist: "Kanye West",
+        avg_review: 4.1,
+        album_cover: "/images/vultures.jpg"
+    },
+    3:{
+        name:"noir",
+        artist: "sho",
+        avg_review: 3.7,
+        album_cover: "/images/noir.jpg"
+    }
+}
+
 router.get("/", (req, res) => {
-    res.render("manage-songs");
+    res.render("manage-songs",{songs});
 });
 
 module.exports = router;
