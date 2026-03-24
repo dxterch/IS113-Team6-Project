dadcc831516f2ee8ac03a364703e839a569513b7
rialora <jesslyn.hu@gmail.com>
2026-03-24 15:44:28 +0800

    e

--- routes/playlist-routes.js
+++ routes/playlist-routes.js
@@ -2,8 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 // Display the main Playlist Management dashboard
-router.get("/manage", (req, res) => {
-    res.render("manage-playlist");
+router.get("/manage", async (req, res) => {
+    let playlist = null;
+if (req.params.id){
+    playlist = await playlist.findById(req.params.id).populate('songs');
+}
+    res.render("manage-playlist", {playlist: null});
 });
 
 router.get("/details", (req, res) => {
