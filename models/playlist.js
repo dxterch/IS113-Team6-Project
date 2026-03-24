bd2268a5bc70779522e45366b83b7ef9295538fa
rialora <jesslyn.hu@gmail.com>
2026-03-24 14:41:48 +0800

    Update playlist.js

---
+++ models/playlist.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const playlistSchema = new mongoose.Schema(
+{
+    uid: {
+        type: String,
+        required: true
+    },
+    pname: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    caption:{
+        type: String
+    },
+    dateCreated:{
+        type: Date.now
+    }
+}
+)
+
+const Playlist = mongoose.model("Playlist", playlistSchema, "playlists")
+exports.createPlaylist = (playlists) => {
+    return Playlist.create(playlists);
+}
