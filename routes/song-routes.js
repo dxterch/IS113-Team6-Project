c44ea1567e4c211c4a3e7bed71c4500bb8bdf8da
lilniblet <rithik.p.2025@computing.smu.edu.sg>
2026-03-29 15:00:47 +0800

    minor change

--- routes/song-routes.js
+++ routes/song-routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const songController = require("../controllers/song-controller")
 const authMiddleware = require("../middlewares/auth-middleware");
 
-router.get("/",authMiddleware.requireLogin ,songController.showSongs);
-router.post("/",authMiddleware.requireLogin, songController.searchSongs);
+router.get("/browse",authMiddleware.requireLogin ,songController.showSongs);
+router.post("/browse",authMiddleware.requireLogin, songController.searchSongs);
 
 module.exports = router;
